8298d8162d8dd160d9c9030009e7f3f9d5cff6b1
Brlr2003 <102907796+Brlr2003@users.noreply.github.com>
2022-04-17 16:54:03 +0200

    Delete application directory

--- application/checkPassword.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-if ($_POST['password'] == '1234')
-    header('Location: admin.php');
-else
-    echo 'not hi';
--- application/password.php
+++
@@ -1,7 +0,0 @@
-<html>
-<form action="checkPassword.php" method="POST">
-    Enter the password: <input name="password" type="password" required>
-    <input type="submit">
-</form>
-
-</html>
